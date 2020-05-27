79381adb729811662bb4aa2df8271e40188a8b3a
kaiwanghuang <417362459@qq.com>
2020-05-27 10:16:18 +0800

    first commit

---
+++ 系统音效iOS/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  系统音效iOS
+//
+//  Created by BDKid on 2020/5/27.
+//  Copyright © 2020 BDKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ 系统音效iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  系统音效iOS
+//
+//  Created by BDKid on 2020/5/27.
+//  Copyright © 2020 BDKid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
